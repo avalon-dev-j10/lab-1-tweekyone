cb7b11f6cd5909799ca7678b579b08f3a8c2144c
Программирование на Java <JAVA@avalon.ru>
2019-03-30 12:54:35 +0300

    Правки на практическом занятии

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -1,12 +1,17 @@
 package ru.avalon.java.dev.j10.labs;
 
 import ru.avalon.java.dev.j10.labs.models.Person;
+import ru.avalon.java.dev.j10.labs.commons.Address;
 
 public class Main {
     public static void main (String args[]){
         //создание экземпляров имен
         Person ivanov = new Person("Иванов","Иван","Иванович");
         Person smith = new Person("John","Edvard","Smith");
+        
+        //создание экземпляров адресов
+        ivanov.setAddress("Россия", "Санкт-Петербург", "Ленина", 17, 9);
+        smith.setAddress("USA", "Los Angeles", "Lenina", 3, 12);
 
         //создание строковых переменных
         String ivanovFullName = ivanov.getFullName();
